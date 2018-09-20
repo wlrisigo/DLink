fbaeb32e0e8e3ea8c275739fa8fb66e1d7011712
Billy Risigo <wlrisigo@gmail.com>
2018-09-20 16:53:20 -0400

    got rid of testing

--- dlist.wrisigo.c
+++ dlist.wrisigo.c
@@ -135,101 +135,11 @@ void printList(DListNode *theList){
 	}
 }
 
-void test_1D() {
-
-	DListNode *theList;
-	int i;
-	theList = NULL;
-	for (i=1; i<=10; ++i) {
-		insertAtEnd(&theList, i);
-	}
-	printList(theList);
-	printf(" %s\n ", "");
-	// should get 1 2 3 4 5 6 7 8 9 10
-	for (i=20; i>=0; i=i-2) {
-
-		if ( isInList(theList, i) ) {
-			deleteFromList(&theList, i);
-		} // 9-17-18 I forgot this bracket
-	}
-	printList(theList);
-	// should get 1 3 5 7 9
-}
-
-
-
-void test_2D() {
-	DListNode *theList;
-	int i;
-	theList = NULL;
-	for (i=1; i<=10; ++i) {
-		if (i%2 == 0)
-			insertSorted(&theList, i);
-		else
-			insertSorted(&theList, 10-i);
-	}
-	printList(theList);
-	// should get 1 2 3 4 5 6 7 8 9 10
-	for (i=1; i<=9; ++i) {
-		if (i%2 == 0)
-			deleteFromList(&theList, i);
-		else
-			deleteFromList(&theList, 10-i);
-	}
-	printList(theList);
-	// should get 10
-}
-
-
-void test_3D() {
-	DListNode *theList;
-	int i;
-	theList = NULL;
-	for (i=1; i<=10; ++i) {
-		if (i < 5)
-			insertSorted(&theList, 1); // 9-17-18 I changed this
-		else
-			insertSorted(&theList, 10);
-	}
-	printList(theList);
-	printf(" %s\n ", "");
-	// should get 1 1 1 1 10 10 10 10 10 10
-	 deleteFromList(&theList, 1);
-	 printf(" %s\n ", "");
-	 printList(theList);
-	 // should get 10 10 10 10 10 10
-	 deleteFromList(&theList, 2);
-	 printList(theList);
-	 printf(" %s\n ", "");
-	 // should get 10 10 10 10 10 10
-	 deleteFromList(&theList, 10);
-	 printList(theList);
-	 printf(" %s\n ", "");
-	 // should get nothing printed for the list
-	}
-
-
-	void test_4D() {
-	 DListNode *theList; // 9-17-18 I forgot a semicolon here
-	 int i;
-	 theList = NULL;
-	 for (i=1; i<=10; ++i) {
-	 if (i%2 == 0)
-	 insertSorted(&theList, i);
-	 else
-	 insertSorted(&theList, 10-i);
-	 }
-	 printList(theList);
-	 // should get 1 2 3 4 5 6 7 8 9 10
-	 printListReverse(theList);
-	 // should get 10 9 8 7 6 5 4 3 2 1
-	}
 
 
 
 	int main(int argc, char *argv[])
 	{
-		test_4D();
 		return(0);
 	}
 
